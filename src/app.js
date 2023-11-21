ab977426e6aa28d209d4f290656727accd7272cb
indiana <indimar.w1@gmail.com>
2023-11-21 11:47:31 +0100

    listo dia3

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require("express")
 const cors = require('cors')
-// const booksRouters = require("./router/books.routers")
+const booksRouters = require("./router/books.routers")
 const book1Routers = require("./router/book1.router")
 const errorHandling = require("./error/errorHandling")
 
 const app = express();
 
 
-app.set("port", process.env.PORT || 2000)
+app.set("port", process.env.PORT || 3000)
 
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-// app.use(booksRouters);
+app.use(booksRouters);
 app.use(book1Routers);
 app.use(function(req, res, next){
     res.status(404).json({error:true,
